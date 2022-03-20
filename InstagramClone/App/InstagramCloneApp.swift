a94037ceef2451eecb63930fe4e3ade4538d8b85
Shubham Kumar <kumar.shubham1329@gmail.com>
2022-03-20 11:32:12 +0530

    Firebase Config

--- InstagramClone/App/InstagramCloneApp.swift
+++ InstagramClone/App/InstagramCloneApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct InstagramCloneApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
